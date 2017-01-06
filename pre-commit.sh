a1c3c16f44a6718892bf3d13346bf4d991d821ae
Alexey Cherkashin <goodniceweb@gmail.com>
2017-01-06 19:39:22 +0300

    Update wording on pre commit hook

--- pre-commit.sh
+++ pre-commit.sh
@@ -14,7 +14,7 @@ FORBIDDEN='binding.pry\|byebug'
 git diff --cached --name-only | \
     grep -E $FILES_PATTERN | \
     GREP_COLOR='4;5;37;41' xargs grep --color --with-filename -n $FORBIDDEN && \
-    echo 'COMMIT REJECTED' && \
+    echo "Looks like you are trying to commit something you shouldn't.  Please fix your diff, or run 'git commit --no-verify' to skip this check, if you must." && \
     exit 1
  
 exit 0
